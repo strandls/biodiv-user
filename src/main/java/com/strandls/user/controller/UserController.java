cdea0e1cba9e79e64bd9d5012d880b96ac7a060b
Abhishek Kumar Rudra <abhishek@darbhanga.strandls.com>
2019-11-26 19:31:05 +0530

    :recycle: observationCreate

--- src/main/java/com/strandls/user/controller/UserController.java
+++ src/main/java/com/strandls/user/controller/UserController.java
@@ -3,21 +3,17 @@
  */
 package com.strandls.user.controller;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import org.pac4j.core.profile.CommonProfile;
-
 import com.google.inject.Inject;
 import com.strandls.authentication_utility.filter.ValidateUser;
-import com.strandls.authentication_utility.util.AuthUtil;
 import com.strandls.user.ApiConstants;
 import com.strandls.user.pojo.User;
 import com.strandls.user.pojo.UserIbp;
@@ -49,17 +45,17 @@ public class UserController {
 	}
 
 	@GET
+	@Path("/{userId}")
 	@Consumes(MediaType.TEXT_PLAIN)
 	@Produces(MediaType.APPLICATION_JSON)
-	@ValidateUser
 	@ApiOperation(value = "Find User by User ID", notes = "Returns User details", response = User.class)
 	@ApiResponses(value = { @ApiResponse(code = 404, message = "Traits not found", response = String.class) })
 
-	public Response getUser(@Context HttpServletRequest request) {
+	public Response getUser(@PathParam("userId") String userId) {
 
 		try {
-			CommonProfile profile = AuthUtil.getProfileFromRequest(request);
-			Long uId = Long.parseLong(profile.getId());
+
+			Long uId = Long.parseLong(userId);
 			User user = userSerivce.fetchUser(uId);
 			return Response.status(Status.OK).entity(user).build();
 		} catch (Exception e) {
@@ -68,17 +64,16 @@ public class UserController {
 	}
 
 	@GET
-	@Path(ApiConstants.IBP)
+	@Path(ApiConstants.IBP + "/{userId}")
 	@Consumes(MediaType.TEXT_PLAIN)
 	@Produces(MediaType.APPLICATION_JSON)
 	@ValidateUser
 	@ApiOperation(value = "Find User by User ID for ibp", notes = "Returns User details", response = UserIbp.class)
 	@ApiResponses(value = { @ApiResponse(code = 404, message = "Traits not found", response = String.class) })
 
-	public Response getUserIbp(@Context HttpServletRequest request) {
+	public Response getUserIbp(@PathParam("userId") String userId) {
 		try {
-			CommonProfile profile = AuthUtil.getProfileFromRequest(request);
-			Long id = Long.parseLong(profile.getId());
+			Long id = Long.parseLong(userId);
 			UserIbp ibp = userSerivce.fetchUserIbp(id);
 			return Response.status(Status.OK).entity(ibp).build();
 		} catch (Exception e) {
