04e4c73b32b7b58195cb0a1c194a8ff5ce1f012b
Abhishek Kumar Rudra <abhishek@darbhanga.strandls.com>
2019-12-16 17:17:28 +0530

    :recycle: Added new route-me

--- src/main/java/com/strandls/user/controller/UserController.java
+++ src/main/java/com/strandls/user/controller/UserController.java
@@ -3,16 +3,22 @@
  */
 package com.strandls.user.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.pac4j.core.profile.CommonProfile;
+
 import com.google.inject.Inject;
+import com.strandls.authentication_utility.filter.ValidateUser;
+import com.strandls.authentication_utility.util.AuthUtil;
 import com.strandls.user.ApiConstants;
 import com.strandls.user.pojo.User;
 import com.strandls.user.pojo.UserIbp;
@@ -47,7 +53,7 @@ public class UserController {
 	@Path("/{userId}")
 	@Consumes(MediaType.TEXT_PLAIN)
 	@Produces(MediaType.APPLICATION_JSON)
-	
+
 	@ApiOperation(value = "Find User by User ID", notes = "Returns User details", response = User.class)
 	@ApiResponses(value = { @ApiResponse(code = 404, message = "Traits not found", response = String.class) })
 
@@ -78,6 +84,26 @@ public class UserController {
 			return Response.status(Status.OK).entity(ibp).build();
 		} catch (Exception e) {
 			return Response.status(Status.BAD_REQUEST).build();
+		}
+	}
+
+	@GET
+	@Path(ApiConstants.ME)
+	@Produces(MediaType.APPLICATION_JSON)
+
+	@ValidateUser
+
+	@ApiOperation(value = "Find the Current user Details", notes = "Returns the Current User Details", response = User.class)
+	@ApiResponses(value = { @ApiResponse(code = 404, message = "User not found", response = String.class) })
+
+	public Response getCurretUser(@Context HttpServletRequest request) {
+		try {
+			CommonProfile profile = AuthUtil.getProfileFromRequest(request);
+			Long uId = Long.parseLong(profile.getId());
+			User user = userSerivce.fetchUser(uId);
+			return Response.status(Status.OK).entity(user).build();
+		} catch (Exception e) {
+			return Response.status(Status.BAD_REQUEST).entity(e.getMessage()).build();
 		}
 	}
 }
