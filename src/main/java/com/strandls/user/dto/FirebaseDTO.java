50455992a9e3e16690cac7c9f3d66bd9240bbf69
Sethuraman Srinivasan <sethu10121994@gmail.com>
2020-06-30 10:21:41 +0530

    :recycle: Modified content-type to json (#32)

---
+++ src/main/java/com/strandls/user/dto/FirebaseDTO.java
@@ -0,0 +1,35 @@
+package com.strandls.user.dto;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@XmlRootElement
+@JsonInclude(value = Include.NON_NULL)
+public class FirebaseDTO {
+	
+	private String token;
+	private String title;
+	private String body;
+	
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+}
