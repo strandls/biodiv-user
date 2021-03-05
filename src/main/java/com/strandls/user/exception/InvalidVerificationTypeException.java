2c806eccbd70d107b965515f043624ee84efbbe5
Abhishek Kumar Rudra <abhishek.rudra02@gmail.com>
2021-03-05 11:35:24 +0530

    fix: profile pic with icon (#59)

--- src/main/java/com/strandls/user/exception/InvalidVerificationTypeException.java
+++ src/main/java/com/strandls/user/exception/InvalidVerificationTypeException.java
@@ -1,6 +1,12 @@
 package com.strandls.user.exception;
 
+
 public class InvalidVerificationTypeException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1615149012175378605L;
+
 	public InvalidVerificationTypeException(String s) {
 		super(s);
 	}
--- src/main/java/com/strandls/user/exception/UserNotFoundException.java
+++ src/main/java/com/strandls/user/exception/UserNotFoundException.java
@@ -1,7 +1,12 @@
 package com.strandls.user.exception;
 
 public class UserNotFoundException extends Exception {
-	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -115522087485447136L;
+
 	public UserNotFoundException(String exception) {
 		super(exception);
 	}
