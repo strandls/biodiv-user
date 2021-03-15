25ed25be0155c34b918dfa1d1a7a01087cbc61bd
akrudra <abhishek.rudra02@gmail.com>
2021-03-15 15:47:17 +0530

    random number generation changed

--- src/main/java/com/strandls/user/util/AppUtil.java
+++ src/main/java/com/strandls/user/util/AppUtil.java
@@ -1,16 +1,14 @@
 package com.strandls.user.util;
 
 import java.net.URISyntaxException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.HttpHeaders;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.http.client.utils.URIBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -90,11 +88,10 @@ public class AppUtil {
 	}
 
 	public static String generateOTP() {
-		Random random = null;
 		try {
-			random = SecureRandom.getInstanceStrong();
-			return String.format("%06d", random.nextInt(1000000));
-		} catch (NoSuchAlgorithmException e) {
+			String random = RandomStringUtils.randomNumeric(6);
+			return random;
+		} catch (Exception e) {
 			log.error(e.getMessage());
 		}
 		return null;
